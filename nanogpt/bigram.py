d85595d86ad7af0fb274eeb8cd1875fdb90e6ef1
Denis Tananaev <d.d.tananaev@gmail.com>
2024-10-10 07:37:47 +0200

    added bigram example

---
+++ nanogpt/bigram.py
@@ -0,0 +1,89 @@
+#
+# Author: Denis Tananaev
+# Date:  09.10.2024
+#
+import os
+import torch
+from functools import partial
+from nanogpt.get_tiny_shakespear import get_dataset
+from nanogpt.bigram_model import BigramLanguageModel
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def get_batch(data_tuple, batch_size, block_size, split):
+    """Generate a small batch of data of inputs x and targets y.
+    
+    Args:
+        data: the data list
+        batch_size: size of batch
+        block_size: seq length
+
+    Returns:
+        x: the inputs of the shape [batch, block_size]
+        y: the targets of the shape [batch, block_size]
+    """
+
+    train, val = data_tuple
+    data = train if split=="train" else val
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(DEVICE), y.to(DEVICE)
+    return x, y
+
+
+@torch.no_grad()
+def estimate_loss(model, get_batch_func, eval_iters):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch_func(split=split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+def train(batch_size = 32,
+        block_size = 8,
+        max_iters = 3000,
+        eval_interval = 300,
+        learning_rate = 1e-2,
+        eval_iters = 200):
+    """Train script.
+    
+    Args:
+        batch_size: how many independent sequences will we process in parallel?
+        block_size: what is the maximum context length for predictions?
+    """
+    torch.manual_seed(1337)
+    train_data, val_data, vocab_size, encode, decode = get_dataset(split=0.9)
+    get_batch_func  = partial(get_batch, data_tuple=(train_data, val_data), batch_size=batch_size, block_size=block_size)
+
+    model = BigramLanguageModel(vocab_size).to(DEVICE)
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    # Training:  The initial loss should be -ln(1/vocab_size) 
+    for iter in range(max_iters):
+
+
+        # every once in a while evaluate the loss on train and val sets
+        if iter % eval_interval == 0:
+            losses = estimate_loss(model, get_batch_func=get_batch_func, eval_iters=eval_iters)
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+        xb, yb = get_batch_func(split="train")
+        logits, loss = model(xb, yb)
+
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    idx  = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
+    print(decode(model.generate(idx, max_new_tokens=400)[0].tolist())) 
+
+if __name__ =="__main__":
+    train()
--- nanogpt/get_tiny_shakespear.py
+++ nanogpt/get_tiny_shakespear.py
@@ -4,6 +4,33 @@
 #
 import os
 import urllib.request
+import torch
+
+def get_dataset(split=0.9):
+     # Get data
+    current_dir = os.getcwd()
+    dataset = get_tiny_shakespear(output_directory=current_dir)
+    # Preprocess
+    chars = sorted(list(set(dataset)))
+    vocab_size = len(chars)
+    # Create simple tokenizer
+    s2i = {ch: idx  for  idx, ch  in enumerate(chars)}
+    i2s = {idx: ch for idx, ch in enumerate(chars)}
+
+    # encoder and decoder 
+    # Google uses sentencepiece:  https://github.com/google/sentencepiece
+    # OpenAI uses tiktoken: https://github.com/openai/tiktoken
+    encode = lambda s: [s2i[ch]  for ch in s] # encoder: take a string and outputs list of tokens
+    decode = lambda i: "".join([i2s[num] for num in i]) # decoder: gets list of tokens and outputs string
+
+    data = torch.tensor(encode(dataset), dtype=torch.long)
+
+    # Dataset split
+    n = int(split * len(data))
+    train_data =data[:n]
+    val_data = data[n:]
+    return train_data, val_data, vocab_size, encode, decode
+
 
 def get_tiny_shakespear(output_directory: str, filename: str = "input.txt"):
     """Loads tiny shakespear data from web."""
